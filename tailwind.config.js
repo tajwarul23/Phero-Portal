ae7536e331894e9fcace47a276caa1006e80d97b
tajwarul23 <tajwarulchowdhury@gmail.com>
2022-05-10 15:09:39 +0600

    banner-done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/hero-pattern.svg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    },
   },
   daisyui: {
     themes: [
